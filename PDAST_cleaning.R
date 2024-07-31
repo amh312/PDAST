08fcb08618ecfaa9c3050691b395f6d7f73d7857
amh312 <127774411+amh312@users.noreply.github.com>
2024-07-31 16:43:05 +0100

    Update PDAST_cleaning.R

--- PDAST_cleaning.R
+++ PDAST_cleaning.R
@@ -1,33 +1,4 @@
 #CLEANING
-options(error=NULL)
-
-##Packages
-
-library("tidyverse")
-library("MLmetrics")
-library("ROCR")
-library("lme4")
-library("rstanarm")
-library("DescTools")
-library("cmdstanr")
-library("posterior")
-library("rethinking")
-library("AMR")
-library("caret")
-library("data.table")
-library("devtools")
-library("MIMER")
-library("corrplot")
-library("glue")
-library("pak")
-library("touch")
-library("sna")
-library("coin")
-
-##Working directory
-
-setwd("/Users/alexhoward/Documents/Projects/UDAST_code")
-path_to_data <- "/Users/alexhoward/Documents/Projects/UDAST_code"
 
 ##Functions
 
