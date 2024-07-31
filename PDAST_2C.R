f2b53e8a30479fe80a0b3440c96bde4ca053ee76
amh312 <127774411+amh312@users.noreply.github.com>
2024-07-31 16:40:28 +0100

    Delete PDAST_2C.R

--- PDAST_2C.R
+++
@@ -1,6 +0,0 @@
-setwd("#FILEPATH#")
-
-#MAKE DATASET FOR PYTHON SCRIPT AND REFERENCE DATAFRAMES
-urines_aware <- read_csv("urines_assess.csv")
-daily_urines <- tibble(urines_aware %>% ungroup() %>% select(subject_id,micro_specimen_id,pAMPr:pTPN))
-write_csv(daily_urines,"#FILEPATH#/daily_urines.csv")
