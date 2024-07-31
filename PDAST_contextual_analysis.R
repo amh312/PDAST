7012f1c6ec28180cd3c9ec6f14d15a241c73f038
amh312 <127774411+amh312@users.noreply.github.com>
2024-07-31 16:48:01 +0100

    Update PDAST_contextual_analysis.R

--- PDAST_contextual_analysis.R
+++ PDAST_contextual_analysis.R
@@ -1,10 +1,17 @@
 #CONTEXTUAL ANALYSIS (PRESCRIBED ANTIMICROBIALS AND IVOST)
-options(error=NULL)
 
-##Packages
+##Functions
 
-library("tidyverse")
-library("AMR")
+###Counting antimicrobial-result matches per paneL
+totaller <- function(panel) {
+  abx_graph %>% filter(Panel==panel) %>% summarise(Total=sum(n)) %>% unlist()
+}
+
+###Counting Access antimicrobial-result matches per paneL
+awaretotaller <- function(panel) {
+  awaresum %>% filter(Panel==panel & AWaRe=="Access" & Result=="S") %>%
+    select(n_results) %>% unlist()
+}
 
 ##Load in required dataframes
 
@@ -35,7 +42,6 @@ urines_abx[,18:30] <- urref
 
 ###Find prescribed antimicrobial-result matches
 print(urines_abx[1,grepl("STANDARD",colnames(urines_abx))] %>% unlist())
-
 urines_abx <- urines_abx %>% 
   mutate(on_standard = case_when(as.ab(abx_name)==STANDARD_1 ~ NIT,
                                  as.ab(abx_name)==STANDARD_2 ~ GEN,
@@ -103,7 +109,7 @@ abx_prescribed <- ggplot(abx_graph, aes(x = abx_name, y = if_else(Panel == "Stan
         legend.text = element_text(size = 12)) +
   scale_x_discrete(expand = expansion(mult = c(0.1, 0.2))) +
   geom_hline(yintercept = 0,linetype="solid",color="black") +
-  ggtitle("Results provided for the antimicrobial agent prescribed (inpatients only)") +
+  ggtitle("Results provided for the antimicrobial agent prescribed (inpatients)") +
   geom_text(x=13.5,y=-100,label="Standard approach",color="#3C3C3C",size=4) +
   geom_text(x=13.5,y=100,label="Personalised approach",color="#3C3C3C",size=4) +
   theme(plot.title = element_text(size = 16, margin = margin(b = 20)),
@@ -117,9 +123,6 @@ ggsave("abx_prescribed.pdf", plot = abx_prescribed, device = "pdf", width = 10, 
        path="/Users/alexhoward/Documents/Projects/UDAST_code")
 
 ###Counting the total number of antimicrobial-result matches per paneL
-totaller <- function(panel) {
-  abx_graph %>% filter(Panel==panel) %>% summarise(Total=sum(n)) %>% unlist()
-}
 PDAST_total <- totaller("PDAST")
 Standard_total <- totaller("Standard")
 glue("The personalised approach provided {PDAST_total} results for antimicrobial
@@ -133,10 +136,6 @@ abx_graph <- abx_graph %>%
 awaresum <- abx_graph %>% group_by(Panel,Result,AWaRe) %>% 
   summarise(n_results=sum(n)) %>% ungroup()
 print(awaresum)
-awaretotaller <- function(panel) {
-  awaresum %>% filter(Panel==panel & AWaRe=="Access" & Result=="S") %>%
-    select(n_results) %>% unlist()
-}
 PDAST_S <- awaretotaller("PDAST")
 Standard_S <- awaretotaller(("Standard"))
 glue("The personalised approach provided {PDAST_S} 'S' results for Access category
@@ -171,25 +170,20 @@ urines_abx <- urines_abx %>% rowwise() %>%
                                 TRUE ~ FALSE))
 n_ac_switch_pdast <- sum(urines_abx$PDAST_ac_alt)
 n_ac_switch_standard <- sum(urines_abx$Standard_ac_alt)
-ivs_only <- c("CRO","TZP","FEP","MEM","CAZ","GEN","CZO")
+ivs_only <- c("CRO","TZP","FEP","MEM","CAZ")
 urines_abx <- urines_abx %>% mutate(PDAST_gen_out = case_when(
   PDAST_ac_alt & NIT!="S"&SXT!="S"&AMP!="S"&SAM!="S"&CZO!="S" ~FALSE,
   TRUE~TRUE
 ),
-Standard_gen_out = case_when(
-  Standard_ac_alt & NIT!="S"&SXT!="S"&AMP!="S"&SAM!="S"&CZO!="S" ~FALSE,
-  TRUE~TRUE
-),
 PDAST_ivost = case_when(abx_name %in% ab_name(ivs_only) &
-  PDAST_ac_alt & NIT!="S"&SXT!="S"&AMP!="S"&SAM!="S" ~FALSE,
-  TRUE~TRUE
+  PDAST_ac_alt & NIT=="S"|SXT=="S"|AMP=="S"|SAM=="S" ~TRUE,
+  TRUE~FALSE
 ),
 Standard_ivost = case_when( abx_name %in% ab_name(ivs_only) &
-  Standard_ac_alt & NIT!="S"&SXT!="S"&AMP!="S"&SAM!="S" ~FALSE,
-  TRUE~TRUE
+  Standard_ac_alt & NIT=="S"|SXT=="S"|AMP=="S"|SAM=="S" ~TRUE,
+  TRUE~FALSE
 ))
-n_pdast_no_cn <- sum(urines_abx$PDAST_gen_out)
-n_standard_no_cn <- sum(urines_abx$Standard_gen_out)
+
 n_pdast_ivost <- sum(urines_abx$PDAST_ivost)
 n_standard_ivost <- sum(urines_abx$Standard_ivost)
 
@@ -197,64 +191,10 @@ glue("The personalised approach provided an opportunity to switch from a
      prescribed Watch category agent to an Access category agent in {n_ac_switch_pdast}
      instances, compared to {n_ac_switch_standard} instances using the standard approach.
      
-     When gentamicin was excluded, the personalised approach provided
-     {n_pdast_no_cn} such opportunities compared to {n_standard_no_cn} with the
-     standard approach.
-     
      The personalised approach provided an step-down from an IV agent to an Access
      category oral agent in {n_pdast_ivost} cases compared to {n_standard_ivost} cases
      using the standard approach
      
-     ")
-
-##Analysis of results provided by urine infection diagnosis type
-
-###Counting Access results provided for patients with UTI codes that admission
-urine_codes <- d_icd_diagnoses %>% filter(grepl("Urinary tract infection",long_title,ignore.case=T) |
-                                            grepl("Acute pyelonephritis",long_title,ignore.case=T) |
-                                            (grepl("Urinary",long_title,ignore.case=T)&
-                                               grepl("Catheter",long_title,ignore.case=T)&
-                                               grepl("Infection",long_title,ignore.case=T))) %>% 
-  distinct(long_title,.keep_all=T)
-diagnoses_icd <- diagnoses_icd %>% semi_join(urines_diags,by="hadm_id") %>% 
-  left_join(urine_codes,by=c("icd_code","icd_version")) %>% 
-  filter(!is.na(long_title))
-diagnosis_key <- diagnoses_icd %>% select(hadm_id,long_title)
-urines_diags <- urines_diags %>% left_join(diagnosis_key,by="hadm_id") %>% 
-  filter(!is.na(long_title))
-top_diagnosis <- urines_diags %>% count(long_title) %>% arrange(desc(n)) %>% slice(1) %>%
-  select(1) %>% unlist()
-top_number <- urines_diags %>% count(long_title) %>% arrange(desc(n)) %>% slice(1) %>%
-  select(2) %>% unlist()
-total_number <- nrow(urines_diags)
-med_p <- median(urines_diags$n_acS_PDAST6)
-iqr1_p <- quantile(urines_diags$n_acS_PDAST6)[2]
-iqr3_p <- quantile(urines_diags$n_acS_PDAST6)[4]
-med_s <- median(urines_diags$n_acS_standard6)
-iqr1_s <- quantile(urines_diags$n_acS_standard6)[2]
-iqr3_s <- quantile(urines_diags$n_acS_standard6)[4]
-med_p_all <- median(urines_diags$n_allS_PDAST6)
-iqr1_p_all <- quantile(urines_diags$n_allS_PDAST6)[2]
-iqr3_p_all <- quantile(urines_diags$n_allS_PDAST6)[4]
-med_s_all <- median(urines_diags$n_allS_standard6)
-iqr1_s_all <- quantile(urines_diags$n_allS_standard6)[2]
-iqr3_s_all <- quantile(urines_diags$n_allS_standard6)[4]
-p_ac <- wilcox.test(urines_diags$n_acS_standard6,urines_diags$n_acS_PDAST6,paired = T,conf.int=T)[3] #significance test
-p_all <- wilcox.test(urines_diags$n_allS_standard6,urines_diags$n_allS_PDAST6,paired = T,conf.int=T)[3] #significance test
-
-glue("{total_number} admissions had a UTI-related icd code, {top_number} of which were
-      '{top_diagnosis}'. The remaining {total_number-top_number} were a mixture of 
-       catheter-related urinary tract infections and pyelonephritis.
-     
-     For urine specimens taken during these admissions, the personalised approach provided
-     a median of {med_p} (IQR {iqr1_p}-{iqr3_p}) Access 'S' results per specimen, versus
-     a median of {med_s} (IQR {iqr1_s}-{iqr3_s}) Access 'S' results per specimen for the standard approach
-     (P={p_ac}).
-     
-     For urine specimens taken during these admissions, the personalised approach provided
-     a median of {med_p_all} (IQR {iqr1_p_all}-{iqr3_p_all}) total 'S' results per specimen, versus
-     a median of {med_s_all} (IQR {iqr1_s_all}-{iqr3_s_all}) total 'S' results per specimen for the standard approach
-     (P={p_all}).
      ")
 
 
