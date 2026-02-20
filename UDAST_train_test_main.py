924d6224aa6cd8a1bfe18a95f732c499e5af6c38
Alex Howard <127774411+amh312@users.noreply.github.com>
2026-02-20 08:09:35 +0000

    #filepath added

--- UDAST_train_test_main.py
+++ UDAST_train_test_main.py
@@ -12,7 +12,7 @@ to_drop = ["subject_id","micro_specimen_id"]
 metrics_fairness = ['Accuracy','TPR','FPR','FNR','PPP']
 
 ###Preprocessing
-ref_df = pd.read_csv("urines5b.csv")
+ref_df = pd.read_csv("#FILEPATH#/urines5b.csv")
 ref_df['standard_age'] = ref_df['standard_age'].map(str)
 dummy_df = pd.get_dummies(ref_df.drop(drops, axis=1))
 
@@ -34,7 +34,7 @@ aucrocs = {}
 for antimicrobial in list(range(0, len(drops), 1)):
 
     ###Training, hyperparameter tuning, and validation
-    train_test_validate_main(csv="urines5.csv",
+    train_test_validate_main(csv="#FILEPATH#/urines5.csv",
                              abx=drops[antimicrobial],
                              antibiotic=ab_full[antimicrobial],
                              ab=lc_abs[antimicrobial])
@@ -56,25 +56,25 @@ for antimicrobial in list(range(0, len(drops), 1)):
 ##Compiling and exporting results
 
 ###Main analysis coefficients
-urines5.to_csv("urines_coef_df.csv",index=False)
+urines5.to_csv("#FILEPATH#/urines_coef_df.csv",index=False)
 
 for antimicrobial in list(range(0, len(drops), 1)):
-    with open(lc_abs[antimicrobial]+"file.pickle", 'rb') as f:
+    with open("#FILEPATH#/"+lc_abs[antimicrobial]+"file.pickle", 'rb') as f:
         vari_list = pickle.load(f)
-    with open(lc_abs[antimicrobial]+"fits.pickle", 'rb') as f:
+    with open("#FILEPATH#/"+lc_abs[antimicrobial]+"fits.pickle", 'rb') as f:
         model_dict = pickle.load(f)
     df1 = pd.DataFrame({'Parameter': 'Intercept','Value': np.round(model_dict.intercept_,3)})
     df2 = pd.DataFrame({'Parameter': vari_list,'Value': np.round(model_dict.coef_[0],3)})
     df2 = df2.sort_values(by='Value', ascending=False)
     df = pd.concat([df1, df2], ignore_index=True)
-    df.to_csv(drops[antimicrobial]+'_coef_list.csv',index=False)
+    df.to_csv("#FILEPATH#/"+drops[antimicrobial]+'_coef_list.csv',index=False)
 
 ###Main analysis performance metrics
-metrics_df = result_compiler(class_reps,"main_analysis_metrics.csv")
+metrics_df = result_compiler(class_reps,"#FILEPATH#/main_analysis_metrics.csv")
 aucrocs_df = pd.DataFrame(list(aucrocs.items()), columns=['Antimicrobial', 'AUC_ROC'])
 aucrocs_df['Information'] = 'Default'
-aucrocs_df.to_csv("default_aucrocs.csv", index=False)
+aucrocs_df.to_csv("#FILEPATH#/default_aucrocs.csv", index=False)
 
 ###Fairness analysis
-fairness_true_metrics = compile_fairness_results(overallfairT,"T_fairness_metrics.csv")
-fairness_false_metrics = compile_fairness_results(overallfairF,"F_fairness_metrics.csv")
+fairness_true_metrics = compile_fairness_results(overallfairT,"#FILEPATH#/T_fairness_metrics.csv")
+fairness_false_metrics = compile_fairness_results(overallfairF,"#FILEPATH#/F_fairness_metrics.csv")
